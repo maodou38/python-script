09be3df90d9824e6bdcb6b3061e0e00fbf9fd280
maodou38 <maodoulove19950815@vip.qq.com>
2020-02-19 18:12:42 +0800

    更新配置

--- wait_until_buy.py
+++ wait_until_buy.py
@@ -20,22 +20,11 @@ from email.utils import formataddr
 # 第三方 SMTP 服务
 my_sender='rio_maodou@sina.com'    # 发件人邮箱账号
 my_pass = 'b6d5a0c923552897'       # 发件人邮箱密码
-
-# ==== 设定抢购时间 （修改此处，指定抢购时间点）====
-#BUY_TIME = "2020-02-17 19:59:58"
-
 # ====  标识登录状态、重试次数 ====
 MAX_LOGIN_RETRY_TIMES = 10
 
 current_retry_login_times = 0
 login_success = False
-#buy_time_object = datetime.datetime.strptime(BUY_TIME, '%Y-%m-%d %H:%M:%S')
-
-now_time = datetime.datetime.now()
-'''if now_time > buy_time_object:
-    print("当前已过抢购时间，请确认抢购时间是否填错...")
-    exit(0)
-'''
 print("正在打开chrome浏览器...")
 #让浏览器不要显示当前受自动化测试工具控制的提醒
 option = webdriver.ChromeOptions()
@@ -47,6 +36,7 @@ print("chrome浏览器已经打开...")
 
 def __login_operates():
     driver.get("https://www.taobao.com")
+    time.sleep(2)
     try:
         if driver.find_element_by_link_text("亲，请登录"):
             print("没登录，开始点击登录按钮...")
@@ -80,46 +70,60 @@ def login():
     now = datetime.datetime.now()
     print('login success:', now.strftime('%Y-%m-%d %H:%M:%S'))
 
-def buy(url,to_send):
-    #打开购物车
-    driver.get(url)
-    time.sleep(2)
-    flag=True
-    #确定该页面商品是否失效
-    while flag:
-        try:
-            if driver.find_element_by_class_name("sold-out-left"):
-                print("页面商品失效，准备刷新页面重试")
-                time.sleep(10)
-                driver.get(url)
-        except:
-            print("页面有效，开始购买")
+def _refresh_keep_alive(BUY_TIME):
+    buy_time_object = datetime.datetime.strptime(BUY_TIME, '%Y-%m-%d %H:%M:%S')
+    now_time = datetime.datetime.now()
+    if (buy_time_object-now_time).seconds <0:
+        print("请指定未来时间")
+        exit(0)
+    if (buy_time_object-now_time).seconds >180:
+        driver.get("https://www.taobao.com")
+        time.sleep(60)
+        return False
+    else:
+        return True
+
+def buy(url,to_send,BUY_TIME):
+    while _refresh_keep_alive(BUY_TIME) :
+        #打开购物车
+        driver.get(url)
+        time.sleep(2)
+        flag=True
+        #确定该页面商品是否失效
+        while flag:
             try:
-                if driver.find_element_by_class_name("J_TSaleProp"):
-                    print("找到商品选择标签")
-                    driver.find_element_by_css_selector(".J_TSaleProp > li:first-child > a").click()#选中首个商品并点击
-                    print("选中商品")
-                    flag=False
-                    driver.find_element_by_id("J_LinkBuy").click() #点击立即购买
-                    #等待页面跳转
-                    has_jump=False
-                    while not has_jump:
-                        try:
-                            if driver.find_element_by_link_text("提交订单"):
-                               has_jump=True
-                               driver.find_element_by_link_text("提交订单").click()
-                        except:
-                            time.sleep(0.2)
-                    #未完待续   邮件通知
-                    i=1
-                    while i<10 :
-                       ret=mail(to_send)
-                       i=i+1
-                       if(ret):
-                           print("邮件发送成功")
-                           break
+                if driver.find_element_by_class_name("sold-out-left"):
+                    print("页面商品失效，准备刷新页面重试")
+                    time.sleep(10)
+                    driver.get(url)
             except:
-                pass
+                print("页面有效，开始购买")
+                try:
+                    if driver.find_element_by_class_name("J_TSaleProp"):
+                        print("找到商品选择标签")
+                        driver.find_element_by_css_selector(".J_TSaleProp > li:first-child > a").click()#选中首个商品并点击
+                        print("选中商品")
+                        flag=False
+                        driver.find_element_by_id("J_LinkBuy").click() #点击立即购买
+                        #等待页面跳转
+                        has_jump=False
+                        while not has_jump:
+                            try:
+                                if driver.find_element_by_link_text("提交订单"):
+                                   has_jump=True
+                                   driver.find_element_by_link_text("提交订单").click()
+                            except:
+                                time.sleep(0.2)
+                        #未完待续   邮件通知
+                        i=1
+                        while i<10 :
+                            ret=mail(to_send)
+                        i=i+1
+                        if(ret):
+                            print("邮件发送成功")
+                            break
+                except:
+                    pass
 def mail(to_send):
     ret=True
     try:
@@ -135,7 +139,11 @@ def mail(to_send):
         ret=False
     return ret
 if __name__=="__main__":
-   url=input("请输入商品链接:")
-   to_send=input("请输入通知邮箱:")
+   #url=input("请输入商品链接:")
+   url="https://m.tb.cn/h.VcRBKqg?sm=ef5e3c"
+   #BUY_TIME=input("几点开始轮候抢购:")
+   BUY_TIME="2020-02-19 18:12:00"
+   #to_send=input("请输入通知邮箱:")
+   to_send="122568712@qq.com"
    login()
-   buy(url,to_send)
+   buy(url,to_send,BUY_TIME)
